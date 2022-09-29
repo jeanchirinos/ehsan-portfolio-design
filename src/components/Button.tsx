76afcb3d3d05f8408d67dfadd20517d8cbc18a53
Jean <JeanChirinos17@gmail.com>
2022-09-29 18:53:34 -0500

    it worked

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,28 +5,16 @@ interface Props {
 }
 
 export default function Button({ children }: Props) {
-  async function handleClick() {
-    // $('dialog')?.classList.add('open')
-    // const mainInput = $('#contact-email') as HTMLInputElement
-    // mainInput.focus()
-    // let items = await navigator.clipboard.read()
-    // for (let item of items) {
-    //   if (!item.types.includes('text/html')) continue
-    //   let reader = new FileReader()
-    //   reader.addEventListener('load', loadEvent => {
-    //     document.getElementById('html-output').innerHTML = reader.result
-    //   })
-    //   reader.readAsText(await item.getType('text/html'))
-    //   break
-    // }
-    navigator.clipboard.readText().then(clipText => alert(clipText))
+  function handleClick() {
+    $('dialog')?.classList.add('open')
+
+    const mainInput = $('#contact-email') as HTMLInputElement
+    mainInput.focus()
   }
 
   return (
-    <div id='html-output'>
-      <button className='btn' onClick={handleClick}>
-        {children}
-      </button>
-    </div>
+    <button className='btn' onClick={handleClick}>
+      {children}
+    </button>
   )
 }

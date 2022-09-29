b3e6817728b0b2581d6d40f00fb11016fd722dac
Jean <JeanChirinos17@gmail.com>
2022-09-29 18:52:06 -0500

    just testing navigator.clipboard.read() in https

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,21 +7,19 @@ interface Props {
 export default function Button({ children }: Props) {
   async function handleClick() {
     // $('dialog')?.classList.add('open')
-
     // const mainInput = $('#contact-email') as HTMLInputElement
     // mainInput.focus()
-
-    let items = await navigator.clipboard.read()
-    for (let item of items) {
-      if (!item.types.includes('text/html')) continue
-
-      let reader = new FileReader()
-      reader.addEventListener('load', loadEvent => {
-        document.getElementById('html-output').innerHTML = reader.result
-      })
-      reader.readAsText(await item.getType('text/html'))
-      break
-    }
+    // let items = await navigator.clipboard.read()
+    // for (let item of items) {
+    //   if (!item.types.includes('text/html')) continue
+    //   let reader = new FileReader()
+    //   reader.addEventListener('load', loadEvent => {
+    //     document.getElementById('html-output').innerHTML = reader.result
+    //   })
+    //   reader.readAsText(await item.getType('text/html'))
+    //   break
+    // }
+    navigator.clipboard.readText().then(clipText => alert(clipText))
   }
 
   return (
